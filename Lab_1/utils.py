5cca0a7293cae1f37989933201c8b127c146a834
Barbara Suslova <barbara.suslova@deelvin.com>
2024-09-14 09:46:54 +0300

    move preprocessing to another file

--- Lab_1/utils.py
+++ Lab_1/utils.py
@@ -1,7 +1,22 @@
 import pandas as pd
 from datetime import datetime
+from typing import Optional
 
-def save_submission(submission: pd.DataFrame, output_path: str) -> None:
+def save_submission(submission: pd.DataFrame, output_path: str, additional_name: Optional[str]) -> None:
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    submission.to_csv(output_path + f"sub_{timestamp}.csv", index=False)
+    if additional_name:
+        file_name = f"sub_{additional_name}_{timestamp}.csv"
+    else:
+        file_name = f"sub_{timestamp}.csv"
+    submission.to_csv(output_path + file_name, index=False)
+
+
+def preprocessing(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.drop(columns=["Unnamed: 0", "Case_ID"])
+
+    df = df.replace({"--": None, "not reported": None})
+    df = df.fillna(-1)
+
+    df["Age_at_diagnosis"] = df["Age_at_diagnosis"].apply(lambda x: int(x.split(" ")[0]) if isinstance(x, str) else x)
+    return df
